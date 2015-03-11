7b9adeb1fd347171be8440d7678628dc060c562c
hi-hu <mike@wealthfront.com>
2015-03-10 21:56:37 -0700

    blank project

---
+++ MiniMeet/MiniMeet/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MiniMeet
+//
+//  Created by Hi_Hu on 3/10/15.
+//  Copyright (c) 2015 hi_hu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
