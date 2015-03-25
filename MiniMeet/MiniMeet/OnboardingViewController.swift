63ae94b066e2c829940492d0543b0831ca2c7d10
Michelle Venetucci <michelle.ann.harvey@gmail.com>
2015-03-24 21:45:57 -0700

    Create final onboarding flow

--- MiniMeet/MiniMeet/OnboardingViewController.swift
+++ MiniMeet/MiniMeet/OnboardingViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class OnboardingViewController: UIViewController, UIScrollViewDelegate {
 
 
+    @IBOutlet weak var browseButton: UIButton!
     @IBOutlet weak var createScreen: UIImageView!
     @IBOutlet weak var welcomeFirstImage: UIView!
     @IBOutlet weak var pageControl: UIPageControl!
@@ -27,11 +28,12 @@ class OnboardingViewController: UIViewController, UIScrollViewDelegate {
         
         
         scrollView.contentSize = feedView.frame.size
-        createScreen.alpha = 0
         mainScrollView.delegate = self
         mainScrollView.contentSize = CGSize(width: 1280, height: 568)
         
-
+        scrollViewDidEndDecelerating(mainScrollView)
+        
+        browseButton.alpha = 0
         // Do any additional setup after loading the view.
     }
     
@@ -42,51 +44,28 @@ class OnboardingViewController: UIViewController, UIScrollViewDelegate {
         // Dispose of any resources that can be recreated.
     }
     
-    @IBAction func startButton(sender: AnyObject) {
-        self.performSegueWithIdentifier("feedSegue", sender: self)
-    }
 
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
+        println("seguing")
     }
-    */
     
-//    func animateUp() {
-//        var imagePosition = self.phoneImage.center.y
-//        var feedPosition = self.feedView.center.y
-//        
-//        UIView.animateWithDuration(0.8, delay: 0.8, usingSpringWithDamping: 0.5, initialSpringVelocity: 40, options: UIViewAnimationOptions.CurveEaseIn, animations: { () -> Void in
-//            self.phoneImage.center.y = imagePosition - 30
-//            self.feedView.center.y = feedPosition - 30
-//            self.phoneImage.alpha = 1
-//            self.feedView.alpha = 1
-//            
-//        }) { (bool) -> Void in
-//            
-//            UIView.animateWithDuration(2, delay: 0, options: UIViewAnimationOptions.CurveEaseIn, animations: { () -> Void in
-//                self.feedView.center.y = feedPosition - 500
-//
-//            }, completion: { (bool) -> Void in
-//                //
-//            })
-//            
-//        }
-//    }
     
-    func scrollViewDidScroll(scrollView: UIScrollView) {
+
+    
+    func didChangePage() {
         var page = Int(mainScrollView.contentOffset.x / 320)
         pageControl.currentPage = page
         
         var imagePosition = self.phoneImage.center.y
         var feedPosition = self.feedView.center.y
+        var createPosition = self.createScreen.center.y
         
         
-        if (page == 1) {
+        if (page == 0) {
             UIView.animateWithDuration(0.8, delay: 0.8, usingSpringWithDamping: 0.5, initialSpringVelocity: 40, options: UIViewAnimationOptions.CurveEaseIn, animations: { () -> Void in
                 self.phoneImage.center.y = imagePosition - 30
                 self.feedView.center.y = feedPosition - 30
@@ -96,18 +75,49 @@ class OnboardingViewController: UIViewController, UIScrollViewDelegate {
                 }) { (bool) -> Void in
                     //
             }
-        } else if (page == 2) {
+        } else if (page == 1) {
             
-            UIView.animateWithDuration(2, delay: 0, options: UIViewAnimationOptions.CurveEaseIn, animations: { () -> Void in
-                self.feedView.center.y = feedPosition - 500
+            UIView.animateWithDuration(2, delay: 0, options: UIViewAnimationOptions.CurveEaseInOut, animations: { () -> Void in
+                self.scrollView.contentOffset.y = feedPosition + 100
+                
                 
                 }, completion: { (bool) -> Void in
                     //
             })
+        } else if (page == 2) {
+            
+            UIView.animateWithDuration(0.3, delay: 0, options: UIViewAnimationOptions.CurveEaseInOut, animations: { () -> Void in
+                
+               
+                self.createScreen.center.y = createPosition - 258
+                
+                }, completion: { (bool) -> Void in
+                self.scrollView.contentOffset.y = feedPosition - 500
+            })
+            
         } else if (page == 3) {
-            createScreen.alpha = 1
+            browseButton.alpha = 1
         }
     }
+    
+    func scrollViewDidEndDecelerating(scrollView: UIScrollView) {
+        didChangePage()
+    }
+    
+    func scrollViewDidEndDragging(scrollView: UIScrollView, willDecelerate decelerate: Bool) {
+        
+        if decelerate == false {
+            didChangePage()
+        }
+    }
+    
+    
+    
+    @IBAction func browseButtonDidPress(sender: AnyObject) {
+        println("test")
+    }
+    
+    
 
 
 }
