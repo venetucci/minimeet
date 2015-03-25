32edc17ff3d1f8066c6855d2b4eb6adb5417debb
Michelle Venetucci <michelle.ann.harvey@gmail.com>
2015-03-24 20:46:42 -0700

    Adding horizontal scroll to onboarding

--- MiniMeet/MiniMeet/OnboardingViewController.swift
+++ MiniMeet/MiniMeet/OnboardingViewController.swift
@@ -8,9 +8,13 @@
 
 import UIKit
 
-class OnboardingViewController: UIViewController {
+class OnboardingViewController: UIViewController, UIScrollViewDelegate {
 
 
+    @IBOutlet weak var createScreen: UIImageView!
+    @IBOutlet weak var welcomeFirstImage: UIView!
+    @IBOutlet weak var pageControl: UIPageControl!
+    @IBOutlet weak var mainScrollView: UIScrollView!
     @IBOutlet weak var phoneImage: UIImageView!
     @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var feedView: UIImageView!
@@ -20,13 +24,18 @@ class OnboardingViewController: UIViewController {
         
         phoneImage.alpha = 0
         feedView.alpha = 0
-        animateUp()
+        
         
         scrollView.contentSize = feedView.frame.size
+        createScreen.alpha = 0
+        mainScrollView.delegate = self
+        mainScrollView.contentSize = CGSize(width: 1280, height: 568)
         
 
         // Do any additional setup after loading the view.
     }
+    
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -47,26 +56,58 @@ class OnboardingViewController: UIViewController {
     }
     */
     
-    func animateUp() {
+//    func animateUp() {
+//        var imagePosition = self.phoneImage.center.y
+//        var feedPosition = self.feedView.center.y
+//        
+//        UIView.animateWithDuration(0.8, delay: 0.8, usingSpringWithDamping: 0.5, initialSpringVelocity: 40, options: UIViewAnimationOptions.CurveEaseIn, animations: { () -> Void in
+//            self.phoneImage.center.y = imagePosition - 30
+//            self.feedView.center.y = feedPosition - 30
+//            self.phoneImage.alpha = 1
+//            self.feedView.alpha = 1
+//            
+//        }) { (bool) -> Void in
+//            
+//            UIView.animateWithDuration(2, delay: 0, options: UIViewAnimationOptions.CurveEaseIn, animations: { () -> Void in
+//                self.feedView.center.y = feedPosition - 500
+//
+//            }, completion: { (bool) -> Void in
+//                //
+//            })
+//            
+//        }
+//    }
+    
+    func scrollViewDidScroll(scrollView: UIScrollView) {
+        var page = Int(mainScrollView.contentOffset.x / 320)
+        pageControl.currentPage = page
+        
         var imagePosition = self.phoneImage.center.y
         var feedPosition = self.feedView.center.y
         
-        UIView.animateWithDuration(0.8, delay: 0.8, usingSpringWithDamping: 0.5, initialSpringVelocity: 40, options: UIViewAnimationOptions.CurveEaseIn, animations: { () -> Void in
-            self.phoneImage.center.y = imagePosition - 30
-            self.feedView.center.y = feedPosition - 30
-            self.phoneImage.alpha = 1
-            self.feedView.alpha = 1
-            
-        }) { (bool) -> Void in
+        
+        if (page == 1) {
+            UIView.animateWithDuration(0.8, delay: 0.8, usingSpringWithDamping: 0.5, initialSpringVelocity: 40, options: UIViewAnimationOptions.CurveEaseIn, animations: { () -> Void in
+                self.phoneImage.center.y = imagePosition - 30
+                self.feedView.center.y = feedPosition - 30
+                self.phoneImage.alpha = 1
+                self.feedView.alpha = 1
+                
+                }) { (bool) -> Void in
+                    //
+            }
+        } else if (page == 2) {
             
             UIView.animateWithDuration(2, delay: 0, options: UIViewAnimationOptions.CurveEaseIn, animations: { () -> Void in
                 self.feedView.center.y = feedPosition - 500
-
-            }, completion: { (bool) -> Void in
-                //
+                
+                }, completion: { (bool) -> Void in
+                    //
             })
-            
+        } else if (page == 3) {
+            createScreen.alpha = 1
         }
     }
+
 
 }
