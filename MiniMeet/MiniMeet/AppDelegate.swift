ab626de8d04b62061b1829f1ce03e1507c1ef397
hi-hu <mike@wealthfront.com>
2015-03-25 17:20:29 -0700

    finish-backing out stuff

--- MiniMeet/MiniMeet/AppDelegate.swift
+++ MiniMeet/MiniMeet/AppDelegate.swift
@@ -17,7 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
 
-        // set the status bar style to light
+        // set the status bar style to lightgit
         UIApplication.sharedApplication().statusBarStyle = .LightContent
         
         // set gloabl tint color
